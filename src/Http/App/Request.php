20a6bd7d7aee73c41692500e28d8ff6f21466aef
sander758 <sander.elderman@home.nl>
2016-05-11 12:52:33 +0200

    Fixed middleware returning and added something for getting POST and GET variables

--- src/Http/App/Request.php
+++ src/Http/App/Request.php
@@ -6,6 +6,16 @@ class Request
 {
     public $route_parameters = array();
 
+    public $POST;
+
+    public $GET;
+
+    function __construct()
+    {
+        $this->POST = $_POST;
+        $this->GET = $_GET;
+    }
+
     /**
      * @param string $value
      * @return mixed|null
